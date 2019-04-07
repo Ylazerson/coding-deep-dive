01f12da84888683355d586852840afcdd986b719
j.lazerson <jake.lazerson@valuecentric.com>
2019-04-07 04:10:07 -0400

    BH - windows functions

---
+++ sql/sql-hw-tasks/sql-assignments/vscode-bq-test.sql
@@ -0,0 +1,10 @@
+-- B''H --
+
+
+SELECT   * 
+FROM     `stats-learning.proj_calendar_yi.secular_days_off_final` 
+
+
+SELECT   year_civil
+FROM     `stats-learning.proj_calendar_yi.secular_days_off_final` 
+limit 5
