8e1e8beed21d6898bfb3ccdb8055d05f05e26bf9
j.lazerson <jake.lazerson@valuecentric.com>
2019-05-26 05:25:19 -0400

    BH - sql task 22  - thank you Hashem

--- sql/sql-hw-tasks/sql-task-022.md
+++ sql/sql-hw-tasks/sql-task-022.md
@@ -6,19 +6,51 @@
 ---
 
 ### Task
-- The company **Shmorgisboorg Labs** is analyzing the liquor sales in Iowa.
-- They want to see the rolling sum of last 10 liquor bottle sales.
+- The company **Shmorgisboorg Labs** is analyzing the liquor sales in Iowa for **year 2017**.
+- They want to see the 3 day rolling sum of liquor bottle sales.
 - However, they just want to see that top three.
 - They only want to see the data for the Polk, Scott, and Clinton counties.
 - They want to see it pivoted.
 
 ---
 
-- See `bigquery-public-data.iowa_liquor_sales.sales` 
+- See: 
+    - `bigquery-public-data.iowa_liquor_sales.sales` 
+    - https://cloud.google.com/bigquery/docs/reference/standard-sql/date_functions#extract
+
+---
+
 - The result should look like this:
 
-|top_last_10_bottle_sales|clinton|polk|scott|
+|top_rolling_3_day_sum_of_bottle_sales|clinton|polk|scott|
 |---|---|---|---|
-|1|1204|15302|3431|
-|2|1200|15298|3425|
-|3|1198|15296|3425|
+|1|8609|130401|47712|
+|2|7484|123933|45716|
+|3|7299|121511|45500|
+
+
+---
+#### To get you started on right track, see this query:
+
+```sql
+
+with day_level_data as
+(
+select   lower(county) county, 
+         extract(dayofyear from date) day_of_year,
+         -- ---------------------------------------------- 
+         sum(bottles_sold) bottles_sold         
+         -- ---------------------------------------------- 
+from     `bigquery-public-data.iowa_liquor_sales.sales` 
+         -- ----------------------------------------------
+where    lower(county) in ('polk', 'scott', 'clinton')
+  and    extract(year from date) = 2017
+         -- ---------------------------------------------- 
+group by lower(county), 
+         extract(dayofyear from date)    
+)
+select   *
+from     day_level_data
+order by 1, 2, 3
+
+```
--- sql/sql-hw-tasks/sql-task-022.sql
+++ sql/sql-hw-tasks/sql-task-022.sql
@@ -1,38 +1,55 @@
 -- B"H --
 
 
-with stg as
+
+
+with day_level_data as
 (
 select   lower(county) county, 
+         extract(dayofyear from date) day_of_year,
          -- ---------------------------------------------- 
-         sum(bottles_sold) over (    
-              partition by  county         
-              order by      date
-              rows between 10 preceding and current row
-          ) last_10_bottle_sales
+         sum(bottles_sold) bottles_sold         
          -- ---------------------------------------------- 
 from     `bigquery-public-data.iowa_liquor_sales.sales` 
          -- ----------------------------------------------
 where    lower(county) in ('polk', 'scott', 'clinton')
+  and    extract(year from date) = 2017
+         -- ---------------------------------------------- 
+group by lower(county), 
+         extract(dayofyear from date)    
+),
+-- -------------------------------------------------------
+rolling_view as
+(
+select   county, 
+         day_of_year,
+         -- ---------------------------------------------- 
+         sum(bottles_sold) over (    
+              partition by  county         
+              order by      day_of_year
+              range between 3 preceding and current row
+         ) rolling_3_day_sum_of_bottle_sales
+         -- ---------------------------------------------- 
+from     day_level_data
 ),
 -- -------------------------------------------------------
 ranks as
 (
 select   county, 
          -- ----------------------------------------------
-         row_number() over (partition by county order by last_10_bottle_sales desc) top_last_10_bottle_sales,
+         row_number() over (partition by county order by rolling_3_day_sum_of_bottle_sales desc) top_rolling_3_day_sum_of_bottle_sales,
          -- ----------------------------------------------
-         last_10_bottle_sales
+         rolling_3_day_sum_of_bottle_sales
          -- ----------------------------------------------
-from     stg
+from     rolling_view
 )
-select   top_last_10_bottle_sales,
+select   top_rolling_3_day_sum_of_bottle_sales,
          -- ----------------------------------------------
-         sum(case when county = 'clinton' then last_10_bottle_sales else 0 end) clinton,
-         sum(case when county = 'polk'    then last_10_bottle_sales else 0 end) polk,
-         sum(case when county = 'scott'   then last_10_bottle_sales else 0 end) scott
+         sum(case when county = 'clinton' then rolling_3_day_sum_of_bottle_sales else 0 end) clinton,
+         sum(case when county = 'polk'    then rolling_3_day_sum_of_bottle_sales else 0 end) polk,
+         sum(case when county = 'scott'   then rolling_3_day_sum_of_bottle_sales else 0 end) scott
          -- ----------------------------------------------
 from     ranks
-where    top_last_10_bottle_sales <= 3
-group by top_last_10_bottle_sales
+where    top_rolling_3_day_sum_of_bottle_sales <= 3
+group by top_rolling_3_day_sum_of_bottle_sales
 
